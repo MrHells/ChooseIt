4409b7c0cf930363e36bb2d026ca361947edc24b
fhill <fhill@LAPTOP-07P64FBV>
2019-08-28 15:38:12 -0300

    28/08/2019 Fhillype

--- src/chooseit/LoginController.java
+++ src/chooseit/LoginController.java
@@ -5,16 +5,13 @@
  */
 package chooseit;
 
-import databases.UserDAO;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Button;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
-import javafx.scene.input.InputMethodEvent;
 import tools.Login;
 import users.User;
 
