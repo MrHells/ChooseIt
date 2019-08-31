494d12fdd33f688e40df09b2c2c11a98bfeb2388
MrHells <44823067+MrHells@users.noreply.github.com>
2019-08-30 23:08:48 -0300

    Update QuestionsCreationController.java

--- src/chooseit/QuestionsCreationController.java
+++ src/chooseit/QuestionsCreationController.java
@@ -5,13 +5,21 @@
  */
 package chooseit;
 
+import Ask.Ask;
+import static chooseit.MenuController.user;
+import databases.AskDAO;
 import java.net.URL;
+import java.sql.SQLException;
 import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
+import tools.Archive;
+import users.User;
 
 /**
  * FXML Controller class
@@ -26,18 +34,19 @@ public class QuestionsCreationController implements Initializable {
     private TextField txtStatement;
     @FXML
     private TextField txtCondition;
-
     /**
      * Initializes the controller class.
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
-    }    
-
+    }
     @FXML
     private void submit(ActionEvent event) {
-        
+        try {
+            AskDAO.saveAsk(new Ask(txtStatement.getText(), txtCondition.getText(), 0, 0, user.getId()));
+        } catch (SQLException ex){
+            Logger.getLogger(QuestionsCreationController.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
-    
 }
