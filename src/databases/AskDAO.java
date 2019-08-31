1749a520e3d732f8a28cffe275b8d35479b37886
MrHells <44823067+MrHells@users.noreply.github.com>
2019-08-30 23:01:53 -0300

    Update AskDAO.java

--- src/databases/AskDAO.java
+++ src/databases/AskDAO.java
@@ -12,6 +12,8 @@ import java.sql.SQLException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import Ask.Ask;
+import java.util.ArrayList;
+import java.util.List;
 import users.User;
 
 /**
@@ -19,85 +21,44 @@ import users.User;
  * @author fhill
  */
 public class AskDAO {
+
     private static Connection con = null;
-    
-     public void save_answer_yes() throws SQLException{
+
+    public void save_answer_yes() throws SQLException {
         Connection con = ConnectionFactory.getConnection();
-        String sql =    "UPDATE ask \n" +
-                        "SET yesQuant = yesQuant + 1\n" +
-                        "WHERE idAsk = 1;";
+        String sql = "UPDATE ask \n"
+                + "SET yesQuant = yesQuant + 1\n"
+                + "WHERE idAsk = 1;";
         PreparedStatement stmt = null;
 
         try {
             stmt = (PreparedStatement) con.prepareStatement(sql);
             stmt.executeUpdate();
-            
+
         } catch (SQLException ex) {
             System.err.println("Erro " + ex);
         } finally {
             ConnectionFactory.closeConnection(con, stmt);
         }
     }
-    
-    public void save_answer_no() throws SQLException{
+
+    public void save_answer_no() throws SQLException {
         Connection con = ConnectionFactory.getConnection();
-        String sql =    "UPDATE ask \n" +
-                        "SET notQuant = notQuant + 1\n" +
-                        "WHERE idAsk = 1;";
+        String sql = "UPDATE ask \n"
+                + "SET notQuant = notQuant + 1\n"
+                + "WHERE idAsk = 1;";
         PreparedStatement stmt = null;
 
         try {
             stmt = (PreparedStatement) con.prepareStatement(sql);
             stmt.executeUpdate();
-            
+
         } catch (SQLException ex) {
             System.err.println("Erro " + ex);
         } finally {
             ConnectionFactory.closeConnection(con, stmt);
         }
     }
-    
-    
-    public static String find_statement() throws SQLException{
-         String sql = "SELECT statement FROM chooseit_database.ask ";
-        try {
-             con = ConnectionFactory.getConnection();
-        } catch (SQLException ex) {
-            Logger.getLogger(UserDAO.class.getName()).log(Level.SEVERE, null, ex);
-        }
-        try (PreparedStatement stmt = (PreparedStatement) con.prepareStatement(sql)){
-            ResultSet rs = stmt.executeQuery();
-            if(rs.next()){
-                String tipo = rs.getString("statement");
-                return tipo;
-            }
-
-        } catch (SQLException ex) {
-            System.err.println("Erro " + ex);
-        }
-      return null;
-    }
-    
-    public static String find_condition() throws SQLException{
-         String sql = "SELECT conditions FROM chooseit_database.ask ";
-        try {
-             con = ConnectionFactory.getConnection();
-        } catch (SQLException ex) {
-            Logger.getLogger(UserDAO.class.getName()).log(Level.SEVERE, null, ex);
-        }
-        try (PreparedStatement stmt = (PreparedStatement) con.prepareStatement(sql)){
-            ResultSet rs = stmt.executeQuery();
-            if(rs.next()){
-                String tipo = rs.getString("conditions");
-                return tipo;
-            }
-
-        } catch (SQLException ex) {
-            System.err.println("Erro " + ex);
-        }
-      return null;
-    }
-    
 
     public static void saveAsk(Ask ask) throws SQLException {
         Connection con = ConnectionFactory.getConnection();
@@ -111,29 +72,36 @@ public class AskDAO {
             stmt.setInt(3, 0);
             stmt.setInt(4, 0);
             stmt.setInt(5, ask.getIdCreator());
-            
+
             stmt.executeUpdate();
         } catch (SQLException ex) {
             System.err.println("Erro " + ex);
         } finally {
             ConnectionFactory.closeConnection(con, stmt);
-        }   
-    }
-    
-    public String buscarUsuarios(String Nickname, String Pass) {
-        String sql = "SELECT nickname, name, idUser, Password FROM chooseit_database.user WHERE Nickname = '"+Nickname+"' and Password ='"+Pass+"'";
+        }
+    }//( ͡° ͜ʖ ͡°)
+
+    public List<Ask> searchAsk(int idUser) {
+        String sql = "SELECT statement, conditions, yesQuant, notQuant FROM ask LEFT JOIN answer using(idAsk);";
+        List<Ask> asks = new ArrayList(); 
         try {
             con = (new ConnectionFactory()).getConnection();
         } catch (SQLException ex) {
             Logger.getLogger(UserDAO.class.getName()).log(Level.SEVERE, null, ex);
         }
-        try (PreparedStatement stmt = (PreparedStatement) con.prepareStatement(sql)){
+        try (PreparedStatement stmt = (PreparedStatement) con.prepareStatement(sql)) {
             ResultSet rs = stmt.executeQuery();
-            if(rs.next()){
-                String tipo = rs.getString("Name");
-                tipo += " | " + rs.getString("Nickname");
-                tipo += " | " + rs.getString("idUser");
-                return tipo;
+            while (true) {
+                if (rs.next()) {
+                    String[] data = new String[4];
+                    data[0] = rs.getString("statement");
+                    data[1] = rs.getString("conditions");
+                    data[2] = rs.getString("yesQuant");
+                    data[3] = rs.getString("notQuant");
+                    asks.add(new Ask(data[0], data[1], idUser, Integer.parseInt(data[2]), Integer.parseInt(data[3])));
+                }else{
+                    return asks;
+                }
             }
 
         } catch (SQLException ex) {
@@ -141,23 +109,23 @@ public class AskDAO {
         }
         return null;
     }
-    
-    public static boolean verifyExistence(String Nickname) throws SQLException{
-        String sql = "SELECT Nickname FROM chooseit_database.user WHERE Nickname = '"+Nickname+"'";
+
+    public static boolean verifyExistence(String Nickname) throws SQLException {
+        String sql = "SELECT Nickname FROM chooseit_database.user WHERE Nickname = '" + Nickname + "'";
         boolean r = false;
         try {
             con = (new ConnectionFactory()).getConnection();
         } catch (SQLException ex) {
-             System.err.println("Erro " + ex);
+            System.err.println("Erro " + ex);
         }
-        
-        try (PreparedStatement stmt = (PreparedStatement) con.prepareStatement(sql)){
+
+        try (PreparedStatement stmt = (PreparedStatement) con.prepareStatement(sql)) {
             ResultSet rs = stmt.executeQuery();
-            if(rs.next()){ 
+            if (rs.next()) {
                 System.out.println("I");
                 return true;
             }
-            
+
         } catch (SQLException ex) {
             System.err.println(ex);
         }
